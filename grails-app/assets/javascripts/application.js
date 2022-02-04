b2316d171e15f9230e76fb00a47cd2f3d406dc7d
philippe lhardy <pl@artisanlogiciel.net>
2022-02-04 14:33:29 +0100

    Revert "Usable on mobile wiht jQuery UI Touch Punch"
    This reverts commit 42e3ff974ed00c820758ba9dd638917dc97349c2.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,9 +5,8 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery-1.7.1.min
-//= require jquery-ui.min-1.8.17
-//= require jquery.ui.touch-punch
+//= require jquery-1.11.1.min
+//= require jquery-ui.min-1.11.3
 //= require jquery-picklist.min
 //= require datatables-1.10.2
 //= require jquery.datetimepicker.full.min
